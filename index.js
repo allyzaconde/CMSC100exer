49a8820cdfe9be7a16907a0b5e594681ef8aab48
Allyza Conde <abconde2@up.edu.ph>
2020-11-03 00:40:32 +0800

    feat: add a working server

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-console.log("hello")
+const Fastify = require('fastify');
+
+//initialize server using fastify
+const server = Fastify({
+    logger: true,
+    trustProxy: true
+});
+
+//access root address - http://localhost/
+server.get('/', {
+
+    /**
+     * @param {*} req - request parameter sent by client
+     */
+    handler: async(req) => {
+        console.log("?????//")
+        return{ success: true }
+    }
+});
+
+async function start () {
+    //get port from env variable
+    const port = parseInt(process.env.PORT || '8080');
+    const address = '0.0.0.0';
+
+    const addr = await server.listen(port, address);
+    console.log(`Listening on ${addr}`);
+}
+
+start();
